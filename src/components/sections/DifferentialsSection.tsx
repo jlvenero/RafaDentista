091c69d03aa873db1b37e258c1be656f14d4e415
Lucas Martins venero <lucasmvenero@gmail.com>
2026-07-09 20:17:12 -0300

    fix: remove remaining unused React imports

--- src/components/sections/DifferentialsSection.tsx
+++ src/components/sections/DifferentialsSection.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { diferenciaisData } from '../../constants/data';
 import { fadeInUp, staggerContainer } from '../../utils/animations';
